6d9bbe6a4218bd5bcc54063f3536a48c025da94f
Danny Hurley <hurley.dannyp@gmail.com>
2017-03-10 12:52:47 -0500

    Only displaying each student once.

--- js/randomizer.js
+++ js/randomizer.js
@@ -9,8 +9,24 @@ $(function () {
         "Maren Woodruff"
     ];
 
-    var nextStudentIndex = Math.floor(Math.random() * students.length);
-    var nextStudent = students[nextStudentIndex];
+    function displayNextStudent() {
+        var nextStudentIndex = Math.floor(Math.random() * students.length);
+        var nextStudent = students[nextStudentIndex];
 
-    $("#student-name").html(nextStudent);
+        $("#student-name").html(nextStudent);
+
+        return nextStudentIndex;
+    }
+
+    function removeStudentFromList(studentIndexToRemove) {
+        students.splice(studentIndexToRemove, 1);
+    }
+
+    $("#next-student-button").on("click", function () {
+        var studentIndexToRemove = displayNextStudent();
+
+        removeStudentFromList(studentIndexToRemove);
+    });
+
+
 });
