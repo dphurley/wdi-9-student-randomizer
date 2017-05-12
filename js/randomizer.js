2f135de2412ad048a8d9efdf1c46ad04b85247c5
Daniel Hurley <dphurley@Daniels-MacBook-Pro.local>
2017-05-12 14:14:47 -0400

    Added WDI10 student names.

--- js/randomizer.js
+++ js/randomizer.js
@@ -1,53 +1,35 @@
 $(function () {
 
-    function displayNextStudentName() {
-        var nextStudentIndex = Math.floor(Math.random() * students.length);
-        var nextStudent = students[nextStudentIndex];
+  function displayNextStudentName() {
+    var nextStudentIndex = Math.floor(Math.random() * students.length);
+    var nextStudent = students[nextStudentIndex];
 
-        $("#student-name").html(nextStudent.name);
+    $('#student-name').html(nextStudent.name);
 
-        return nextStudentIndex;
-    }
+    return nextStudentIndex;
+  }
 
-    function removeStudentFromList(studentIndexToRemove) {
-        students.splice(studentIndexToRemove, 1);
-    }
+  function removeStudentFromList(studentIndexToRemove) {
+    students.splice(studentIndexToRemove, 1);
+  }
 
-    $("#next-student-button").on("click", function () {
-        var studentIndex = displayNextStudentName();
+  $('#next-student-button').on('click', function () {
+    var studentIndex = displayNextStudentName();
 
-        removeStudentFromList(studentIndex);
-    });
+    removeStudentFromList(studentIndex);
+  });
 
-    var students = [
-        { name: "Alex Gadd" },
-        { name: "Aylin McGinnis" },
-        { name: "Blake Smith" },
-        { name: "Brandon King" },
-        { name: "Jibril Sulaiman II" },
-        { name: "Charles Wilkinson" },
-        { name: "Dan Milo" },
-        { name: "David Hernquist" },
-        { name: "Dee Davis" },
-        { name: "Erin Martin" },
-        { name: "Felicia Garcia" },
-        { name: "Harry Karambizi" },
-        { name: "Hassan Masroor" },
-        { name: "Jacob Butler" },
-        { name: "Josh Seipel" },
-        { name: "Joy Sutton" },
-        { name: "Julian Ireland Chandler" },
-        { name: "Lanchana Shivananda" },
-        { name: "Liam Robinson" },
-        { name: "Maggie Kinney" },
-        { name: "Matt Atkins" },
-        { name: "Michael Torres" },
-        { name: "Neil Morris" },
-        { name: "Peter Colella" },
-        { name: "Randy Galeano" },
-        { name: "Ray Meibaum" },
-        { name: "Rush Myers" },
-        { name: "Sungmin Ro" }
-    ];
+  var students = [
+    {name: 'Ben Floyd'},
+    {name: 'Chris McClain'},
+    {name: 'Christy Bartholomew'},
+    {name: 'Clark Waite'},
+    {name: 'Jeff Henneberg'},
+    {name: 'Monica Grages'},
+    {name: 'Sarah Moore'},
+    {name: 'William Rausch'},
+    {name: 'Yeni Nomezqui'},
+    {name: 'Kate Montha'},
+  ];
 
 });
